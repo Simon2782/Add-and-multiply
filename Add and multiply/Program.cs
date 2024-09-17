e5be768c41f657fd12194a2bd3b33f478fb6b6a5
Simon2782 <simongus@yahoo.se>
2024-09-17 15:39:06 +0200

    Add project files.

---
+++ Add and multiply/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Add_and_multiply
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vänligen välj två nummer: ");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            int userChoice2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine(Num(userChoice, userChoice2));
+
+            Console.WriteLine(Mult(userChoice, userChoice2));
+
+            Console.WriteLine(Div(userChoice, userChoice2));
+
+            Console.WriteLine(sub(userChoice, userChoice2));
+        }
+        
+        /// <summary>
+        /// Adds the two numbers toghether
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        /// <returns></returns>
+
+        private static int Num(int one, int two)
+
+        {
+
+            int sum = one + two;
+            return sum;
+        }
+
+        private static int Mult(int a, int b)
+        {
+            int sum = a * b;
+            return sum;
+        }
+
+        private static int Div(int first, int second)
+        {
+            int sum = first / second;
+            return sum;
+        }
+
+        private static int sub(int one, int two)
+        {
+            int sum = one - two;
+            return sum; 
+        }
+
+
+        
+    }
+}
